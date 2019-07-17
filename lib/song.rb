b340f9771efcdc5b07ec7a448bb05a5082bcc194
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-17 02:47:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,11 +20,46 @@ class Song
       new_song
     end
 
-    def new_by_name(name)
+    def self.new_by_name(name)
+      song = self.new
+      song.name = name
+      song
+    end
+
+    def self.create_by_name(name)
       song = self.new
       song.name = name
       @@all << song
+      song
     end
+
+    def self.find_by_name(name)
+      @@all.find{|song| song.name == name}
+    end
+
+    def self.find_or_create_by_name(name)
+      self.create_by_name(name)
+      self.find_by_name(name)
+    end
+
+    def self.alphabetical
+        @@all.sort { |a, b| a.name <=> b.name }
+      end
+
+
+      def self.new_from_filename(filename)
+            a = filename.split(" - ")
+            artist_name = a[0]
+            a_one = a[1].split(".")
+            name = a_one[0]
+
+            song.new
+            song.name = name
+            song.artist_name = artist_name
+            
+            
+            
+            end
 
 
 end
